e33410a872d96279fc0c4f27a5ab76f3b798187c
panha <pamela_vs@estudante.sc.senai.br>
2024-04-01 18:27:11 -0300

    :sparkles: atualiza repository de nota

--- src/main/java/com/example/anotacoes/datasource/repository/NotaRepository.java
+++ src/main/java/com/example/anotacoes/datasource/repository/NotaRepository.java
@@ -4,6 +4,9 @@ import com.example.anotacoes.datasource.entity.NotaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotaRepository extends JpaRepository<NotaEntity, Long> {
+    List<NotaEntity> findAllByUsuarioId(Long idUsuario);
 }
