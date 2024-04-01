d72b37133fdba7468ff8c92e4651f16f53165b9c
panha <pamela_vs@estudante.sc.senai.br>
2024-04-01 17:51:15 -0300

    :sparkles: cria service de usuário

--- src/main/java/com/example/anotacoes/controller/UsuarioController.java
+++ src/main/java/com/example/anotacoes/controller/UsuarioController.java
@@ -1,12 +1,10 @@
 package com.example.anotacoes.controller;
 
 import com.example.anotacoes.controller.dto.request.InserirUsuarioRequest;
-import com.example.anotacoes.datasource.entity.UsuarioEntity;
-import com.example.anotacoes.datasource.repository.UsuarioRepository;
+import com.example.anotacoes.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,25 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class UsuarioController {
-    private final BCryptPasswordEncoder bCryptEncoder;
-    private final UsuarioRepository usuarioRepository;
+    private final UsuarioService service;
 
     @PostMapping("/cadastro")
     public ResponseEntity<String> newUser(@RequestBody InserirUsuarioRequest inserirUsuarioRequest) throws Exception {
-        boolean usuarioExistente = usuarioRepository.findByNomeUsuario(inserirUsuarioRequest.nomeUsuario())
-                .isPresent();
-
-        if (usuarioExistente){
-            throw new Exception("Usuário já cadastrado.");
-        }
-
-        UsuarioEntity usuario = new UsuarioEntity();
-        usuario.setNomeUsuario(inserirUsuarioRequest.nomeUsuario());
-        usuario.setSenha(bCryptEncoder.encode(inserirUsuarioRequest.senha()).toString());
-
-        usuarioRepository.save(usuario);
-
-        return new ResponseEntity<>("Criado", HttpStatus.CREATED);
+        service.criarUsuario(inserirUsuarioRequest);
+        return new ResponseEntity<>("Usuário cadastrado com sucesso.", HttpStatus.CREATED);
     }
 
 }
