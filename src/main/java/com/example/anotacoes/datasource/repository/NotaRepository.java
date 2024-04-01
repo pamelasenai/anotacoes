cd83b8cff453fe64e3802e84c2e21546b392fe84
panha <pamela_vs@estudante.sc.senai.br>
2024-04-01 17:29:49 -0300

    :sparkles: cria repository

--- src/main/java/com/example/anotacoes/datasource/repository/NotaRepository.java
+++ src/main/java/com/example/anotacoes/datasource/repository/NotaRepository.java
@@ -2,6 +2,8 @@ package com.example.anotacoes.datasource.repository;
 
 import com.example.anotacoes.datasource.entity.NotaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotaRepository extends JpaRepository<NotaEntity, Long> {
 }
